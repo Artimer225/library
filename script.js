6a62c5cb97cf6030abfad88e3bd073ae4ac19dbe
Artimer225 <horizon225artimer@gmail.com>
2025-08-16 22:20:48 +0800

    Add changes to style and some small adjustments of sizes

--- script.js
+++ script.js
@@ -22,9 +22,9 @@ function BookForDisplay(book) {
     this.pages = 'Pages: ' + book.pages;
     this.language = 'Language: ' + book.language;
     this.status = 'Status: ';
-    if (book.status === 1) {
+    if (book.status === '1') {
         this.status += 'Currently reading';
-    } if (this.status === 2) {
+    } else if (book.status === '2') {
         this.status += 'Read';
     } else {
         this.status += 'Not read';
@@ -34,9 +34,9 @@ function BookForDisplay(book) {
 
 
 // dummy cards for testing purposes
-let firstBook = new Book('The Hobbit', 'J. R. R. Tolkien', 356, 'English', 2);
-let secondBook = new Book('Crime and Punishment', 'Fyodor Dostoevsky', 488, 'Russian', 0)
-let thirdBook = new Book('1984', 'George Orwell', 328, 'English', 2)
+let firstBook = new Book('The Hobbit', 'J. R. R. Tolkien', '356', 'English', '2');
+let secondBook = new Book('Crime and Punishment', 'Fyodor Dostoevsky', '488', 'Russian', '0')
+let thirdBook = new Book('1984', 'George Orwell', '328', 'English', '2')
 
 library.push(firstBook, secondBook, thirdBook);
 
@@ -47,17 +47,60 @@ for (let i = 0; i < library.length; i++) {
 
 let cardholder = document.querySelector('.cardholder');
 
+function createChangeButton(cardButtons) {
+    let changeStatus = document.createElement('div');
+    changeStatus.classList.add('change-status');
+    let changeDialog = document.createElement('dialog');
+    changeDialog.classList.add('change-dialog');
+    let changeStatusForm = document.createElement('form');
+    changeStatusForm.classList.add('change-status-form');
+    let changeForm = document.createElement('div');
+    changeForm.classList.add('change-form');
+    let label = document.createElement('label');
+    label.setAttribute('for', 'change-read');
+    let select = document.createElement('select');
+    select.setAttribute('name', 'status');
+    select.setAttribute('id', 'change-read');
+    select.appendChild(new Option('Not read', '0'));
+    select.appendChild(new Option('Currently reading', '1'));
+    select.appendChild(new Option('Read', '2'));
+    //now create buttons for form
+    let changeFormButtons = document.createElement('div')
+    changeFormButtons.classList.add('change-form-buttons');
+    let cancelButton = document.createElement('input');
+    cancelButton.setAttribute('value', 'Cancel');
+    cancelButton.setAttribute('type', 'submit');
+    let confirmButton = document.createElement('input');
+    confirmButton.setAttribute('value', 'Confirm');
+    confirmButton.setAttribute('type', 'submit');
+    changeFormButtons.appendChild(cancelButton);
+    changeFormButtons.appendChild(confirmButton);
+
+
+    changeStatus.appendChild(changeDialog);
+    changeDialog.appendChild(changeStatusForm);
+    changeStatusForm.appendChild(changeForm);
+    changeForm.appendChild(label);
+    changeForm.appendChild(select);
+    changeStatusForm.appendChild(changeFormButtons);
+
+    //creating button to access this dialog
+    let changeButton = document.createElement('button');
+    changeButton.classList.add('change-btn');
+    changeButton.textContent = 'Change Status';
+    //and now the struggle ends, append it to the changeStatus and then to the cardButtons div. voila!
+    changeStatus.appendChild(changeButton);
+    cardButtons.appendChild(changeStatus);
+}
+
 function addButtons(card) {
     let cardBtns = document.createElement('div');
     cardBtns.classList.add('card-btns');
     let deleteBtn = document.createElement('button');
     deleteBtn.classList.add('delete-btn');
     deleteBtn.textContent = 'Delete Book';
-    let changeBtn = document.createElement('button');
-    changeBtn.classList.add('change-btn');
-    changeBtn.textContent = 'Change Status';
     cardBtns.appendChild(deleteBtn);
-    cardBtns.appendChild(changeBtn);
+    createChangeButton(cardBtns);
     card.appendChild(cardBtns);
 }
 
@@ -74,7 +117,11 @@ function displayLibrary(lib) {
             if (j === 0) {
                 p.classList.add('title');
                 p.innerText = valuesOfBook[j];
-             card.appendChild(p)
+                card.appendChild(p)
+            } else if (j === valuesOfBook.length - 2) {
+                p.classList.add('status');
+                p.innerText = valuesOfBook[j];
+                card.appendChild(p)
             } else if (j === valuesOfBook.length - 1) {
                 card.setAttribute('data-id', valuesOfBook[j]);
             } else {
@@ -90,13 +137,15 @@ function displayLibrary(lib) {
 displayLibrary(formLibrary)
 
 const showDialog = document.getElementById('showDialog');
-const bookDialog = document.getElementById('dialog');
+const bookDialog = document.getElementById('create-dialog');
+const changeDialog = document.getElementById('change-dialog');
 const bookForm = document.getElementById('book-dialog-form');
 const closeBtn = document.getElementById('close-btn');
 
+
 cardholder.addEventListener('click', e => {
     if (e.target.matches('.delete-btn')) {
-            let currentCardId = e.target.parentElement.parentElement.dataset.id;
+            let currentCardId = e.target.closest('.card').dataset.id;
             let currentCard = document.querySelector(`[data-id="${currentCardId}"]`);
             currentCard.remove()
             const isId = (book) => book.id === currentCardId;
@@ -109,7 +158,28 @@ cardholder.addEventListener('click', e => {
 
 cardholder.addEventListener('click', e => {
     if (e.target.matches('.change-btn')) {
-        console.log("you've clicked change button")
+        let currentCardId = e.target.closest('.card').dataset.id;
+        const isId = (book) => book.id === currentCardId;
+        const currentStatus = library[library.findIndex(isId)].status;
+        let parentDiv = e.target.closest('.change-status');
+        parentDiv.querySelector('[name="status"]').options[currentStatus].selected = true;
+        parentDiv.firstChild.showModal();
+    }
+})
+
+cardholder.addEventListener('submit', e => {
+    if (e.target.matches('.change-status-form')) {
+        e.preventDefault();
+        // THIS ONE! finally
+        const chosenStatus = e.target.querySelector('[name="status"]').value;
+        const chosenStatusText = e.target.querySelector(`[value="${chosenStatus}"]`).innerText;
+        let currentCardId = e.target.closest('.card').dataset.id;
+        const isId = (book) => book.id === currentCardId;
+        library[library.findIndex(isId)].status = chosenStatus;
+        let previousStatus = e.target.closest('.card').querySelector('.status').innerText.slice(0, 8);
+        previousStatus += chosenStatusText;
+        e.target.closest('.card').querySelector('.status').innerText = previousStatus;
+
     }
 })
 
